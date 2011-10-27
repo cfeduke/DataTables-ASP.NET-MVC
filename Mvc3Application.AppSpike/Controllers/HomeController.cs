ce642589c53c91692f7623d65d5801d57ca4b990
Johannes Brunner <johannes_brunner@gmx.net>
2011-10-27 09:09:55 +0200

    add mvc 3 app spike with razor engine

---
+++ Mvc3Application.AppSpike/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Datatables.Mvc;
+using System.Text;
+
+namespace Mvc3Application.AppSpike.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetDataTables(DataTable dataTable) {
+            List<List<string>> table = new List<List<string>>();
+
+            List<int> column1 = new List<int>();
+            for (int i = dataTable.iDisplayStart; i < dataTable.iDisplayStart + dataTable.iDisplayLength; i++) {
+                column1.Add(i);
+            }
+
+            foreach (var sortDir in dataTable.sSortDirs) {
+                if (sortDir == DataTableSortDirection.Ascending) {
+                    column1.Sort();
+                } else {
+                    column1.Sort(delegate(int a, int b) {
+                        if (a > b) return -1;
+                        if (a < b) return 1;
+                        return 0;
+                    });
+                }
+            }
+
+            for (int i = 0; i < column1.Count; i++) {
+                table.Add(new List<string> { column1[i].ToString(), "Nummer" + i });
+            }
+
+            var result = new DataTableResult(dataTable, table.Count, table.Count, table);
+            result.ContentEncoding = Encoding.UTF8;
+            return result;
+        }
+
+        public ActionResult Index2() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetDataTables2(DataTable dataTable) {
+            List<List<string>> table = new List<List<string>>();
+
+            List<int> column1 = new List<int>();
+            for (int i = dataTable.iDisplayStart; i < dataTable.iDisplayStart + dataTable.iDisplayLength; i++) {
+                column1.Add(i);
+            }
+
+            foreach (var sortDir in dataTable.sSortDirs) {
+                if (sortDir == DataTableSortDirection.Ascending) {
+                    column1.Sort();
+                } else {
+                    column1.Sort(delegate(int a, int b) {
+                        if (a > b) return -1;
+                        if (a < b) return 1;
+                        return 0;
+                    });
+                }
+            }
+
+            for (int i = 0; i < column1.Count; i++) {
+                table.Add(new List<string> { column1[i].ToString(), "ÄÖÜäöü" + i });
+            }
+
+            var result = new DataTableResult(dataTable, table.Count, table.Count, table);
+            result.ContentEncoding = Encoding.UTF8;
+            return result;
+        }
+    }
+}
