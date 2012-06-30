535321a654a7f833f0f1bf8b4fdbe112c0ac2c57
Charles Feduke <charles.feduke@gmail.com>
2012-06-30 08:31:46 -0400

    working datatables.net implementation

--- Datatables.Mvc/DataTableResult.cs
+++ Datatables.Mvc/DataTableResult.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace Datatables.Mvc {
 
@@ -45,15 +45,18 @@ namespace Datatables.Mvc {
         [DataMember(Name = "sColumns")]
         public string sColumns { get; set; }
 
-        /// <summary>
-        /// The data in a 2D array
-        /// Fill this structure with the plain table data
-        /// represented as string.
-        /// </summary>
-        [DataMember(Name = "aaData")]
-        public List<List<string>> aaData { get; set; }
+		///// <summary>
+		///// The data in a 2D array
+		///// Fill this structure with the plain table data
+		///// represented as string.
+		///// </summary>
+		//[DataMember(Name = "aaData")]
+		//public List<List<string>> aaData { get; set; }
 
-        [IgnoreDataMember]
+		[DataMember(Name = "aaData")]
+		public List<IDictionary<string,string>> aaData { get; set; }
+		
+		[IgnoreDataMember]
         public Encoding ContentEncoding { get; set; }
 
         [IgnoreDataMember]
@@ -62,13 +65,27 @@ namespace Datatables.Mvc {
         [IgnoreDataMember]
         public JsonRequestBehavior JsonRequestBehavior { get; set; }
 
-        public DataTableResult(string sEcho, int iTotalRecords, int iTotalDisplayRecords, List<List<string>> aaData) {
-            this.JsonRequestBehavior = JsonRequestBehavior.DenyGet;
-            this.sEcho = sEcho;
-            this.iTotalRecords = iTotalRecords;
-            this.iTotalDisplayRecords = iTotalDisplayRecords;
-            this.aaData = aaData;
-        }
+        public DataTableResult(string sEcho, int iTotalRecords, int iTotalDisplayRecords, List<List<string>> aaData) : this(sEcho, iTotalRecords, iTotalDisplayRecords) 
+		{
+        	if (aaData == null) return;
+        	var data = new List<IDictionary<string,string>>(aaData.Count);
+        	data.AddRange(aaData.Select(datum => DataTableRow.IndexEnumerable(datum)));
+        	this.aaData = data;
+		}
+
+		public DataTableResult(DataTable dataTable, int iTotalRecords, int iTotalDisplayRecords, IEnumerable<DataTableRow> aaData) : this(dataTable.sEcho, iTotalRecords, iTotalDisplayRecords)
+		{
+			if (aaData == null) return;
+			this.aaData = aaData.Select(datum => datum.ToDictionary()).ToList();
+		}
+
+		private DataTableResult(string sEcho, int iTotalRecords, int iTotalDisplayRecords)
+		{
+			this.JsonRequestBehavior = JsonRequestBehavior.DenyGet;
+			this.sEcho = sEcho;
+			this.iTotalRecords = iTotalRecords;
+			this.iTotalDisplayRecords = iTotalDisplayRecords;
+		}
 
         public DataTableResult(DataTable dataTable, int iTotalRecords, int iTotalDisplayRecords, List<List<string>> aaData)
             : this(dataTable.sEcho, iTotalRecords, iTotalDisplayRecords, aaData) {
@@ -91,19 +108,12 @@ namespace Datatables.Mvc {
                 throw new InvalidOperationException("Get not allowed");
             }
             HttpResponseBase response = context.HttpContext.Response;
-            if (!string.IsNullOrEmpty(this.ContentType)) {
-                response.ContentType = this.ContentType;
-            } else {
-                response.ContentType = "application/json";
-            }
+            response.ContentType = !string.IsNullOrEmpty(this.ContentType) ? this.ContentType : "application/json";
             if (this.ContentEncoding != null) {
                 response.ContentEncoding = this.ContentEncoding;
             }
 
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(DataTableResult));
-            MemoryStream ms = new MemoryStream();
-            serializer.WriteObject(ms, this);
-            string json = response.ContentEncoding.GetString(ms.ToArray());            
+        	var json = JsonConvert.SerializeObject(this);
 
             response.Write(json);
         }
---
+++ Datatables.Mvc/DataTableRow.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Datatables.Mvc
+{
+	/// <summary>
+	/// Represents a single record with associated DT_RowId DT_RowClass attributes.
+	/// </summary>
+	public class DataTableRow
+	{
+		public DataTableRow() { }
+		public DataTableRow(IEnumerable<string> data)
+		{
+			Data = data;
+		}
+		public DataTableRow(string rowId, IEnumerable<string> data) : this(data)
+		{
+			RowId = rowId;
+		}
+		public DataTableRow(string rowId, string rowClass, IEnumerable<string> data) : this(rowId, data)
+		{
+			RowClass = rowClass;
+		}
+		/// <summary>
+		/// Gets or sets the RowId.
+		/// </summary>
+		/// <remarks>When turned into JSON becomes the DT_RowId key/value pair.</remarks>
+		public string RowId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the RowClass.
+		/// </summary>
+		/// <remarks>>When turned into JSON becomes the DT_RowClass key/value pair.</remarks>
+		public string RowClass { get; set; }
+
+		/// <summary>
+		/// Gets or sets fields, in ordinal order.
+		/// </summary>
+		/// <remarks>This data represents a single entry in the aaData construct.</remarks>
+		public IEnumerable<string> Data { get; set; }
+
+		internal IDictionary<string,string> ToDictionary()
+		{
+			const string dtRowIdKey = "DT_RowId";
+			const string dtRowClassKey = "DT_RowClass";
+
+			IDictionary<string,string> result = new Dictionary<string, string>();
+
+			if (!string.IsNullOrEmpty(RowId))
+			{
+				result.Add(dtRowIdKey, RowId);
+			}
+			if (!string.IsNullOrEmpty(RowClass))
+			{
+				result.Add(dtRowClassKey, RowClass);
+			}
+
+			if (Data == null)
+			{
+				return result;
+			}
+			result = IndexEnumerable(Data, result);
+
+			return result;
+		}
+
+		public static IDictionary<string,string> IndexEnumerable(IEnumerable<string> list, IDictionary<string,string> dictionary = null)
+		{
+			if (dictionary == null)
+			{
+				dictionary = new Dictionary<string, string>();
+			}
+
+			var count = 0;
+			foreach (var item in list)
+			{
+				dictionary.Add("" + count, item);
+				count++;
+			}
+
+			return dictionary;
+		} 
+	}
+}
